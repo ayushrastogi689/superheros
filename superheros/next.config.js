ec342d7df9e72da9e6509f69419ef9774f3d6ec6
Ayush Rastogi <ayushrastogi689@gmail.com>
2022-12-18 00:00:39 +0530

    Modified file

--- superheros/next.config.js
+++ superheros/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env:{
-    MONGO_URL:""
+    MONGO_URL:"mongodb+srv://ayushrastogi689:Krishnaradha9988@@//@cluster0.uf0wepi.mongodb.net/?retryWrites=true&w=majority"
   }
 }
 
