a664ce03bd9a4c8e826f21830e6697f870577783
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2022-12-18 00:06:15 +0530

    Deleted file

--- superheros/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env:{
-    MONGO_URL:"mongodb+srv://ayushrastogi689:Krishnaradha9988@@//@cluster0.uf0wepi.mongodb.net/?retryWrites=true&w=majority"
-  }
-}
-
-module.exports = nextConfig
